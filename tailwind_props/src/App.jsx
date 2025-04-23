5e97d3555ff7bc56f63190cdd024eb229aac58fa
Aryan Nawathey <132645349+swirleythrone@users.noreply.github.com>
2025-04-23 18:37:24 +0000

    tutorial done

---
+++ tailwind_props/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj={
+    username:"Aryan",
+    agw:20
+  }
+  let newArr=[1,2,3]
+
+  return (
+    <>
+        <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+        <Card username = "swirley_throne" btnText="click me"/>
+        <Card username = "aryan"/>
+    </>
+  )
+}
+
+export default App
+ 
